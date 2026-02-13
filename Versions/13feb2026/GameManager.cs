9d24334636cb0c8df288ef0e392e68fd04e7c27a
Martin Timothy Timko <complex.indicatrix@gmail.com>
2026-02-13 21:43:03 +0100

    Create GameManager.cs

---
+++ Versions/13feb2026/GameManager.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.Splines;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public enum TerrainMode
+    {
+        None,
+        LevelUp,
+        LevelDown
+    }
+
+    public TerrainMode CurrentTerrainMode { get; private set; }
+
+    public void SetTerrainMode(TerrainMode mode)
+    {
+        CurrentTerrainMode = mode;
+    }
+
+
+    Camera cam;
+
+    private void Start()
+    {
+        instance = this;
+
+        cam = Camera.main;
+
+        //IndicatrixAPI.instance.LoadTxtTiles();
+
+
+
+
+        //-------------- T E S T ---------------------------------------------
+
+
+
+        //IndicatrixAPI.instance.txtSetTile(100, 100, "t");
+
+
+        //IndicatrixAPI.instance.txtSetTile(100, 100, "asd");
+
+
+        //using (StreamWriter sw = new StreamWriter("C:\\game\\0000.txt"))
+        //{
+        //    for (int i = 0; i <= TerrainManager.instance.terrainWidth - 1; i++)
+        //    {
+        //        for (int j = 0; j <= TerrainManager.instance.terrainWidth - 1; j++)
+        //        {
+        //            sw.WriteLine(IndicatrixAPI.instance.txtGetTile(i, j));
+        //        }
+        //    }
+        //}
+
+
+
+        //terrainWidth = 256 - realne je pocet tilov = 256x256 tilov
+
+
+
+        //TO-DO LIST:
+        //1.uloha --- LOAD/SAVE z/do suboru/binarny/ z/do pamate
+        //2.uloha --- vytvorenie MDLtile analogicky ako TXTtile
+        //3.uloha --- do textovej reprezentacie zapisat znak T a podla toho generovat Texturu na danom Tile v mape a vice versa
+        //4.uloha --- modelova reprezentacia
+
+
+
+    }
+
+
+
+
+    private void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = cam.ScreenToWorldPoint(mousePos);
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100000))
+        {
+
+
+
+            //if (Input.GetKeyDown(KeyCode.M))
+            //{
+            //    Vector3 snapCenterFace = IndicatrixAPI.instance.SnapMeshFace(hit.point);
+
+            //}
+
+            //Vector3 snapCenterFace = IndicatrixAPI.instance.SnapMeshFace(hit.point);
+
+       
+
+            Vector3 snapPoint = IndicatrixAPI.instance.SnapVertex(hit.point);
+            
+           // Debug.Log(snapPoint.x + " " + snapPoint.y + " " + snapPoint.z);
+
+
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    switch (CurrentTerrainMode)
+                    {
+                        case TerrainMode.LevelUp:
+                            TerrainManager.instance.TerrainVertexLevel(snapPoint.x, snapPoint.y, snapPoint.z, true);
+                            break;
+
+                        case TerrainMode.LevelDown:
+                            TerrainManager.instance.TerrainVertexLevel(snapPoint.x, snapPoint.y, snapPoint.z, false);
+                            break;
+                    }
+                }
+            }
+
+            //Vector3 snapCenterFace = IndicatrixAPI.instance.SnapMeshFace(hit.point);
+            //Vector3 snapLineFaceCenter = IndicatrixAPI.instance.SnapLineFace(hit.point);
+
+            //Debug.Log("SnapPoint: x = " + snapPoint.x + ", y = " + snapPoint.y + ", z = " + snapPoint.z);
+            //Vector3 snapLineFaceCenter = IndicatrixAPI.instance.SnapLineFace(hit.point);
+
+
+        }
+
+    }
+
+}
